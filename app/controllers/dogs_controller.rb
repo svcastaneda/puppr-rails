f8742ec67ff2172a2135103fab50fbdeda9d6205
svcastaneda <svcastaneda@gmail.com>
2016-03-19 11:16:59 -0500

    Create controllers for dogs, bookings, sessions, and users

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,20 @@
+class DogsController < ApplicationController
+  def index
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def show
+  end
+  
+  def destroy
+  end
+  
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,61 @@
+class UsersController < ApplicationController
+  def index
+    @sitters = User.where(sitter: true)
+  end
+
+  def new
+    if current_user
+      redirect_to edit_profile_path(current_user.profile)
+    else
+      @user = User.new
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to users_path
+    else
+      @errors = @user.errors.full_messages
+      render template: 'users/new'
+    end
+  end
+
+  def show
+    if current_user
+      @user = User.find_by(id: params[:id])
+      if @user.nil? # change to 404
+        redirect_to root_path
+      else
+        redirect_to user_path(@user)
+      end
+    else
+      redirect_to login_path
+    end
+  end
+
+  def edit
+    current_user ? (@user = current_user) : (redirect_to login_path)
+  end
+
+  def update
+    @user = current_user
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user)
+    else
+      @errors = @user.errors.full_messages
+      render template: 'users/edit'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :username, :password, :password_confirmation, :birthday, :price, :xp_years, :housing_type, :bio, :sitter, :address, :city, :state, :country, :zip_code)
+  end
+
+end
