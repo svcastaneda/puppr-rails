5f87ca4c1f77c7bbacec78bf7f42a7eec28d4b2e
svcastaneda <svcastaneda@gmail.com>
2016-03-19 12:17:59 -0500

    Add sitter in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,6 @@
 
 user = User.create(first_name: "Saundra", last_name: "Castaneda", email: "svcastaneda@gmail.com", username: "saundoritos", password: "password", birthday: "1995-12-01", xp_years: 10, housing_type: "apartment", sitter: false, city: "Chicago", state: "IL", country: "USA", zip_code: "60606")
 
+sitter = User.create(first_name: "Adriana", last_name: "Castaneda", email: "adriana@gmail.com", username: "adriana", password: "password", birthday: "1992-07-23", xp_years: 14, housing_type: "apartment", sitter: true, city: "Chicago", state: "IL", country: "USA", zip_code: "60606")
+
 dog = Dog.create(name: "Sammy", birthday: "2008-07-03", breed: "shetland sheepdog", size: "M", owner: user)
