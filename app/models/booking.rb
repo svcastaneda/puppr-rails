1275437425357d80f3f6c5fe052365e3c37b8690
svcastaneda <svcastaneda@gmail.com>
2016-03-19 12:25:27 -0500

    Add date validations to bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,4 +7,23 @@ class Booking < ActiveRecord::Base
   validates :start_date, presence: true
   validates :end_date, presence: true
   
+  validate booking_cant_start_in_the_past, booking_cant_end_in_the_past, booking_cant_start_before_it_ends
+  
+  def booking_cant_start_in_the_past
+    if start_date.present? && start_date < Date.today
+      errors.add(:start_date, "Start date can't be in the past")
+    end
+  end
+  
+  def booking_cant_end_in_the_past
+    if end_date.present? && end_date < Date.today
+      errors.add(:end_date, "End date can't be in the past")
+    end
+  end
+  
+  def booking_cant_start_before_it_ends
+    if start_date.present? && start_date > end_date
+      errors.add(:start_date, "Booking can't start before it ends")
+    end
+  end
 end
