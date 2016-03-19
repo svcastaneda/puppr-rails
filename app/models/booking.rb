5206a4425138fd9a1263f12d6b4f5f6dfb202fed
svcastaneda <svcastaneda@gmail.com>
2016-03-19 12:19:53 -0500

    Add booking validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ActiveRecord::Base
   belongs_to :sitter, foreign_key: :sitter_id, class_name: "User"
-  belongs_to :user, foreign_key: :requester_id, class_name: "User"
+  belongs_to :requester, foreign_key: :requester_id, class_name: "User"
+  
+  validates :sitter_id, presence: true
+  validates :requester_id, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  
 end
