00792f05fa3a127c7a8755d3b08b766c7dc3bc09
svcastaneda <svcastaneda@gmail.com>
2016-04-07 20:28:15 -0500

    Begin creating controller for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,18 +2,31 @@ class BookingsController < ApplicationController
   def index
   end
   
+  def requests_made
+    @requested_bookings = current_user.requested_bookings
+  end
+  
+  def requested_for
+    @requested_for = current_user.booking_requests
+  end
+  
+  # making a request for a dog sitter
   def new
   end
   
+  # making a request for a dog sitter
   def create
   end
   
+  # editing a request made
   def edit
   end
   
+  # show an individual request
   def show
   end
   
+  # cancel a request made
   def destroy
   end
   
